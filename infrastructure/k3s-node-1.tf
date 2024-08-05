d9cb1a6d96c0dd6699bea7b0086b1af0f4def168
G. Rossin <gpl.rgp.pgl@gmail.com>
2024-08-05 22:29:49 +0200

    K3s node vm (#1)
    * K3s node vm
    
    * Add missing providers
    
    * Import k3s node 1

---
+++ infrastructure/k3s-node-1.tf
@@ -0,0 +1,56 @@
+resource "proxmox_vm_qemu" "k3s-node-1" {
+  automatic_reboot       = true
+  balloon                = 0
+  bios                   = "seabios"
+  boot                   = "order=scsi0;net0"
+  cores                  = 4
+  cpu                    = "host"
+  define_connection_info = false
+  full_clone             = false
+  hotplug                = "network,disk,usb"
+  memory                 = 4096
+  name                   = "K3S"
+  onboot                 = true
+  qemu_os                = "l26"
+  scsihw                 = "virtio-scsi-single"
+  tablet                 = true
+  vcpus                  = 0
+  vm_state               = "running"
+  disks {
+    scsi {
+      scsi0 {
+        disk {
+          backup    = true
+          cache     = null
+          format    = "raw"
+          iothread  = true
+          readonly  = false
+          replicate = true
+          size      = "128G"
+          storage   = "local-lvm"
+        }
+      }
+    }
+  }
+  network {
+    bridge    = "vmbr0"
+    firewall  = true
+    link_down = false
+    macaddr   = "BC:24:11:55:00:98"
+    model     = "virtio"
+    mtu       = 0
+    queues    = 0
+    rate      = 0
+    tag       = -1
+  }
+  smbios {
+    uuid = "a26e7393-2fc7-496b-817c-578cedc60556"
+  }
+}
+
+resource "dns_a_record_set" "dns" {
+  zone = "ross.in."
+  name = "k3s-node-1"
+
+  addresses = ["192.168.1.110"]
+}

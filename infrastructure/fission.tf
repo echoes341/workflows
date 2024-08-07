1955a955ff7b28ce9c5a3bf9fc56d934557d88a1
Gianpaolo Rossin <gpl.rgp.pgl@gmail.com>
2024-08-07 15:05:44 +0200

    Install fission

--- infrastructure/fission.tf
+++ infrastructure/fission.tf
@@ -4,10 +4,16 @@ resource "kubernetes_namespace" "fission" {
   }
 }
 
+resource "kubernetes_namespace" "fission_functions" {
+  metadata {
+    name = "functions"
+  }
+}
+
 data "kustomization" "fission_kustomize" {
   provider = kustomization
 
-  path = "github.com/fission/fission/crds/v1?ref=v1.20.1"
+  path = "github.com/fission/fission/crds/v1?ref=v1.20.2"
 }
 
 resource "kustomization_resource" "fission_crds" {
@@ -21,6 +27,8 @@ resource "helm_release" "fission" {
   namespace  = kubernetes_namespace.fission.metadata.0.name
   repository = "https://fission.github.io/fission-charts/"
   chart      = "fission-all"
+  version    = "v1.20.2"
+  depends_on = [kustomization_resource.fission_crds]
 
   set {
     name  = "serviceType"
@@ -29,5 +37,9 @@ resource "helm_release" "fission" {
   set {
     name  = "routerServiceType"
     value = "NodePort"
+  }
+  set {
+    name  = "defaultNamespace"
+    value = kubernetes_namespace.fission_functions.metadata.0.name
   }
 }
